d2c4c20fef700da3ee1cc966c2d971f87bc8e141
Luca Cominardi <luca.cominardi@datadoghq.com>
2026-03-02 10:42:39 +0100

    Add rsync to install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Install system packages
+sudo apt install -y rsync
+
 # Install Rust via rustup (non-interactive)
 if ! command -v rustup &>/dev/null; then
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
